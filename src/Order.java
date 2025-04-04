ce26db8ac4acb58d6abb21493f356e1ee9a6f868
JJ <jayjaychua1018@gmail.com>
2025-04-04 20:23:01 +0800

    Update

--- src/Order.java
+++ src/Order.java
@@ -1,5 +1,18 @@
 
-public class Order 
-{
-
+public class Order {
+	protected String iName, iId, iType;
+	protected double iPrice;
+	protected int iQty;
+	
+	public Order() {
+		
+	}
+	
+	public Order(String iName, String iId, String iType, double iPrice, int iQty) {
+		this.iName = iName;
+		this.iId = iId;
+		this.iType = iType;
+		this.iPrice = iPrice;
+		this.iQty = iQty;
+	}
 }
