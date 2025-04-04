0d43463dd722d877b251a3b8ea1cfcfe0829d556
Siew Wai Choong <siewwaichoong@gmail.com>
2025-04-04 20:56:44 +0800

    arraylist

--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,40 @@
+import java.util.*;
 
 public class Customer 
 {
+	private String userId, userName, userPw;
+	private int custNo;
+	
+	public void Custormer(String userId, String userName, String userPw, int  custNo)
+	{
+		this.userId = userId;
+		this.userName = userName;
+		this.userPw = userPw;
+		this.custNo = custNo;
+	}
+	
+	
+	public void getId(String userId)
+	{
+		
+	}
+	
+	public void getName(String userName)
+	{
+		
+	}
+	
+	public void getPw(String userPw)
+	{
+		
+	}
+	
+	public void getNo(int custNo)
+	{
+		
+	}
+	
+	ArrayList<Customer> al = new ArrayList<Customer>();
+	al.add(new Customer());
 
 }
