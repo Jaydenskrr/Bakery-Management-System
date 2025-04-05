f3cdff97b4455caced23498784715396bb5414a5
Jayden <jayden.hoo61@gmail.com>
2025-04-05 13:12:41 +0800

    y

--- src/Report.java
+++ src/Report.java
@@ -2,37 +2,40 @@ import java.util.*;
 
 public class Report 
 {
+
+    //Attributes for menu items
     protected String iId;
     protected String iName;
     protected String iType;
     protected double iPrice;
     protected int iQty;
 
-    private String reportId;
-    private String dateGenerated;
+    //Attributes for reports
+    // private String reportId;
+    // private String dateGenerated;
 
     public Report(){
 
     }
 
-    public Report(String iId, String iName, String iType, double iPrice, int iQty, String reportId, String dateGenerated) {
+    public Report(String iId, String iName, String iType, double iPrice, int iQty){
         this.iId = iId;
         this.iName = iName;
         this.iType = iType;
         this.iPrice = iPrice;
         this.iQty = iQty;
-        this.reportId = reportId;
-        this.dateGenerated = dateGenerated;
+        // this.reportId = reportId;
+        // this.dateGenerated = dateGenerated;
     }
 
-    public void setData(String iId, String iName, String iType, double iPrice, int iQty, String reportId, String dateGenerated){
+    public void setData(String iId, String iName, String iType, double iPrice, int iQty){
         this.iId = iId;
         this.iName = iName;
         this.iType = iType;
         this.iPrice = iPrice;
         this.iQty = iQty;
-        this.reportId = reportId;
-        this.dateGenerated = dateGenerated;
+        // this.reportId = reportId;
+        // this.dateGenerated = dateGenerated;
 
     }
 
@@ -51,31 +54,23 @@ public class Report
     public int iQty(){
         return iQty;
     }
-    public String reportId(){
-        return reportId;
-    }
-    public String dateGenerated(){
-        return dateGenerated;
-    }
-
 
     // Arraylist to store the bakery menu items
     ArrayList<Report> itemList = new ArrayList<>();
 
     // Method to define and add items to menu
     public void defineItemData(){
-        itemList.add(new Report("1001", "Red Bean Bun", "Bun", 5.99, 20, "R001", "2025-04-04"));
-        itemList.add(new Report("1001", "Chocolate Bun", "Bun", 5.99, 20, "R001", "2025-04-04"));
-        itemList.add(new Report("1001", "Butter Bun", "Bun", 5.99, 20, "R001", "2025-04-04"));
+        itemList.add(new Report("B01", "Red Bean Bun", "Bun", 5.99, 5));
+        itemList.add(new Report("B02", "Chocolate Bun", "Bun", 5.99, 10));
 
-        itemList.add(new Report("1001", "Chocolate Chip Cookie", "Cookie", 5.99, 20, "R001", "2025-04-04"));
-        itemList.add(new Report("1001", "Vanilla Cookie", "Bun", 5.99, 20, "R001", "2025-04-04"));
-        itemList.add(new Report("1001", "Strawberrie Cookie", "Bun", 5.99, 20, "R001", "2025-04-04"));
+        itemList.add(new Report("201", "Chocolate Chip Cookie", "Cookie", 6.99, 15));
+        itemList.add(new Report("202", "Vanilla Cookie", "Cookie", 6.99, 20));
 
-        itemList.add(new Report("1001", "Vanilla Cookie", "Bun", 5.99, 20, "R001", "2025-04-04"));
-        itemList.add(new Report("1001", "Vanilla Cookie", "Bun", 5.99, 20, "R001", "2025-04-04"));
-        itemList.add(new Report("1001", "Vanilla Cookie", "Bun", 5.99, 20, "R001", "2025-04-04")); 
+        itemList.add(new Report("301", "Tiramisu Cake", "Cake", 10.99, 21));
+        itemList.add(new Report("302", "Cheese Cake", "Cake", 10.99, 22));
     }
+
+    
 
 
 
