55e83c9821d2453d321a602304f00275b0af0b03
Siew Wai Choong <siewwaichoong@gmail.com>
2025-04-06 21:24:24 +0800

    sales

--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public abstract class Item 
 {
@@ -5,7 +6,6 @@ public abstract class Item
 	protected String itemType, itemId, itemName;
 	protected int itemQty;
 	protected double itemPrice;
-	
 	
 	public abstract void setItem(String type, String id, String name, int qty, double price);
 		
@@ -32,6 +32,23 @@ public abstract class Item
 	public double getPrice()
 	{
 		return itemPrice;
+	}
+	
+	ArrayList<Report> item = new ArrayList<Report>();
+	
+	public void menu()
+	{
+		item.add(new Report("Bread", "B001", "Baguette", 10, 11.80));
+		item.add(new Report("Bread", "B002", "Focaccia", 20, 12.80));
+		item.add(new Report("Bread", "B003", "Mexico Bun", 8, 6.80));
+		item.add(new Report("Bread", "B004", "Croissant", 10, 10.80));
+		item.add(new Report("Bread", "B005", "Bagel", 5, 5.80));
+		item.add(new Report("Cake", "K001", "Black Forest", 10, 18.80));
+		item.add(new Report("Cake", "K002", "Tiramisu", 5, 23.80));
+		item.add(new Report("Cake", "K003", "Earl Grey", 20, 19.80));
+		item.add(new Report("Cookie", "C001", "Brownie", 10, 9.80));
+		item.add(new Report("Cookie", "C002", "Choco Chip", 10, 9.80));
+
 	}
 	
 }
--- src/Report.java
+++ src/Report.java
@@ -6,12 +6,12 @@ public class Report extends Item
 	private int noSold;
 	private double amtSold;
 	
-	public Report(int noSold, double amtSold)
+	public Report(String type, String id, String name, int qty, double price, int noSold, double amtSold)
 	{
 		
 	}
 	
-	public void setReport(int noSold, double amtSold)
+	public void setReport(String type, String id, String name, int qty, double price, int noSold, double amtSold)
 	{
 		this.noSold = noSold;
 		this.amtSold = amtSold;
@@ -41,31 +41,24 @@ public class Report extends Item
 		itemPrice = price;
 	}
 	
-	ArrayList<Report> item = new ArrayList<Report>();
+	ArrayList<Report> sales = new ArrayList<Report>();
 	
-	public void menu()
+	public void report()
 	{
-		item.add(new Report("Bread", "B001", "Baguette", 10, 11.80));
-		item.add(new Report("Bread", "B002", "Focaccia", 20, 12.80));
-		item.add(new Report("Bread", "B003", "Mexico Bun", 8, 6.80));
-		item.add(new Report("Bread", "B004", "Croissant", 10, 10.80));
-		item.add(new Report("Bread", "B005", "Bagel", 5, 5.80));
-		item.add(new Report("Cake", "K001", "Black Forest", 10, 18.80));
-		item.add(new Report("Cake", "K002", "Tiramisu", 5, 23.80));
-		item.add(new Report("Cake", "K003", "Earl Grey", 20, 19.80));
-		item.add(new Report("Cookie", "C001", "Brownie", 10, 9.80));
-		item.add(new Report("Cookie", "C002", "Choco Chip", 10, 9.80));
+		sales.add(new Report("Bread", "B001", "Baguette", 10, 11.80, 0, 0));
+		sales.add(new Report("Bread", "B002", "Focaccia", 20, 12.80, 0, 0));
+		sales.add(new Report("Bread", "B003", "Mexico Bun", 8, 6.80, 0, 0));
+		sales.add(new Report("Bread", "B004", "Croissant", 10, 10.80, 0, 0));
+		sales.add(new Report("Bread", "B005", "Bagel", 5, 5.80, 0, 0));
+		sales.add(new Report("Cake", "K001", "Black Forest", 10, 18.80, 0, 0));
+		sales.add(new Report("Cake", "K002", "Tiramisu", 5, 23.80, 0, 0));
+		sales.add(new Report("Cake", "K003", "Earl Grey", 20, 19.80, 0, 0));
+		sales.add(new Report("Cookie", "C001", "Brownie", 10, 9.80, 0, 0));
+		sales.add(new Report("Cookie", "C002", "Choco Chip", 10, 9.80, 0, 0));
 
 	}
 	
-	ArrayList<ArrayList<Report>> sales = new ArrayList<ArrayList<Report>>();
 	
-	public void statment()
-	{
-	
-		
-		
-	}
 	
 }
 
