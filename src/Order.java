112dc3bc1d4baa2954b43fcbfefc586a521acca6
JJ <jayjaychua1018@gmail.com>
2025-04-06 14:37:19 +0800

    update order

--- src/Order.java
+++ src/Order.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime; // Get System date and time
 import java.time.format.DateTimeFormatter; // Format date and time
 
 public class Order {
-	protected String orderID;
+	protected String orderID, custID;
 	protected LocalDateTime systemDT;
 	protected double orderPrice;
 	protected int orderQty;
@@ -27,16 +27,20 @@ public class Order {
 	ArrayList<Order> orders = new ArrayList<Order>(); // Create an ArrayList object
 	
 	public void createOrder() {
-		//While loop to add item into cart
+		
 		orderID = ("ORD-" + String.format("%03d", orderCtr));
 		orderCtr++;
 		systemDT = LocalDateTime.now();
-//		Format System Date and Time
+		
+		//Format System Date and Time
 	    DateTimeFormatter formatDT = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 	    String orderDT = systemDT.format(formatDT);
 		
-		System.out.println("Order Id: " + orderID);
+		System.out.println("Order ID: " + orderID);
 		System.out.println("Date/Time: " + orderDT);
+		System.out.println("Customer Name: ");
+		
+		//While loop to add item into cart
 		
 	}
 }
