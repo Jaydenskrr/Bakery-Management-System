5158b94216a6f56958713a38524709407fc27f87
Siew Wai Choong <siewwaichoong@gmail.com>
2025-04-07 17:57:42 +0800

    new

--- src/Item.java
+++ src/Item.java
@@ -1,54 +1,17 @@
 import java.util.ArrayList;
 
-public abstract class Item 
-{
+public abstract class Item{
+	String name;
+	int qty;
+	String id;
+	String type;
 	
-	protected String itemType, itemId, itemName;
-	protected int itemQty;
-	protected double itemPrice;
-	
-	public abstract void setItem(String type, String id, String name, int qty, double price);
-		
-	public String getType()
-	{
-		return itemType;
+	public Item(String id, String name, int qty, String type){
+		this.id = id;
+		this.name = name;
+		this.qty = qty;
+		this.type = type;
 	}
 	
-	public String getId()
-	{
-		return itemId;
-	}
-	
-	public String getName()
-	{
-		return itemName;
-	}
-	
-	public int getQty()
-	{
-		return itemQty;
-	}
-	
-	public double getPrice()
-	{
-		return itemPrice;
-	}
-	
-	ArrayList<Report> item = new ArrayList<Report>();
-	
-	public void menu()
-	{
-		item.add(new Report("Bread", "B001", "Baguette", 10, 11.80));
-		item.add(new Report("Bread", "B002", "Focaccia", 20, 12.80));
-		item.add(new Report("Bread", "B003", "Mexico Bun", 8, 6.80));
-		item.add(new Report("Bread", "B004", "Croissant", 10, 10.80));
-		item.add(new Report("Bread", "B005", "Bagel", 5, 5.80));
-		item.add(new Report("Cake", "K001", "Black Forest", 10, 18.80));
-		item.add(new Report("Cake", "K002", "Tiramisu", 5, 23.80));
-		item.add(new Report("Cake", "K003", "Earl Grey", 20, 19.80));
-		item.add(new Report("Cookie", "C001", "Brownie", 10, 9.80));
-		item.add(new Report("Cookie", "C002", "Choco Chip", 10, 9.80));
-
-	}
 	
 }
--- src/Report.java
+++ src/Report.java
@@ -1,72 +1,31 @@
 import java.util.*;
 
-public class Report extends Item
-{
+public class Report extends Item{
 	
-	private int noSold;
-	private double amtSold;
-	
-	public Report(String type, String id, String name, int qty, double price, int noSold, double amtSold)
-	{
+	public Report(String id, String name, int qty, String type){
+		super(id, name, qty, type);
 		
 	}
-	
-	public void setReport(String type, String id, String name, int qty, double price, int noSold, double amtSold)
-	{
-		this.noSold = noSold;
-		this.amtSold = amtSold;
-	}
-	
-	public int getNoSold()
-	{
-		return noSold;
-	}
-	
-	public double getAmtSold()
-	{
-		return amtSold;
-	}
 
-	public Report(String type, String id, String name, int qty, double price)
-	{
+	ArrayList<Item> view = new ArrayList<Item>();
+	
+	public void sales(){
+		view.add(new Report("A01", "Bread", 10, "bun"));
+	}
+	
+	
+	public void displayReport() {
+		sales();
+		for (int i = 0; i < view.size(); i++) {
+			System.out.println((view.get(i)).toString());
+		}
+	}
+	
+	public static void main(String[] args){
+		Report r = new Report(null, null, 0, null);
 		
+		r.displayReport();
 	}
-	
-	public void setItem(String type, String id, String name, int qty, double price)
-	{
-		itemType = type;
-		itemId = id;
-		itemName = name;
-		itemQty = qty;
-		itemPrice = price;
-	}
-	
-	ArrayList<Report> sales = new ArrayList<Report>();
-	
-	public void report()
-	{
-		sales.add(new Report("Bread", "B001", "Baguette", 10, 11.80, 0, 0));
-		sales.add(new Report("Bread", "B002", "Focaccia", 20, 12.80, 0, 0));
-		sales.add(new Report("Bread", "B003", "Mexico Bun", 8, 6.80, 0, 0));
-		sales.add(new Report("Bread", "B004", "Croissant", 10, 10.80, 0, 0));
-		sales.add(new Report("Bread", "B005", "Bagel", 5, 5.80, 0, 0));
-		sales.add(new Report("Cake", "K001", "Black Forest", 10, 18.80, 0, 0));
-		sales.add(new Report("Cake", "K002", "Tiramisu", 5, 23.80, 0, 0));
-		sales.add(new Report("Cake", "K003", "Earl Grey", 20, 19.80, 0, 0));
-		sales.add(new Report("Cookie", "C001", "Brownie", 10, 9.80, 0, 0));
-		sales.add(new Report("Cookie", "C002", "Choco Chip", 10, 9.80, 0, 0));
-
-	}
-	
 	
 	
 }
-
-    
-
-    
-
-
-
-
-
