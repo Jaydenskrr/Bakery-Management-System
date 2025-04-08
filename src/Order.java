e774f56ab6fd24a9e85d2f2def0836829f2c0814
JJ <jayjaychua1018@gmail.com>
2025-04-09 03:03:06 +0800

    push

--- src/Order.java
+++ src/Order.java
@@ -1,8 +1,32 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 import java.time.LocalDateTime; // Get System date and time
 import java.time.format.DateTimeFormatter; // Format date and time
 
 public class Order {
-
+	
+	private String orderId;
+    private String custPhone;
+    private LocalDateTime time;
+    private double total;
+    private String type; // Online or WalkIn
+    private static int orderCtr = 1;
+    
+    private ArrayList<String> productIds;
+    private ArrayList<String> quantities;
+    
+    
+    public Order() {
+    	
+    }
+    
+    public Order(String custPhone, String type) {
+    	orderId = generateOrderId();
+        this.custPhone = custPhone;
+        time = LocalDateTime.now();
+        this.type = type;
+        productIds = new ArrayList<>();
+        quantities = new ArrayList<>();
+        total = 0.0;
+    }
 }
